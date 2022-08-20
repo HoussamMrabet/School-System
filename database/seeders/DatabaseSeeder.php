39d87743a5678a859038ba7eff0601e25a67cdee
Houssam Mrabet <dodslim100@gmail.com>
2022-08-20 17:21:35 +0100

    modify database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Demande;
+use App\Models\Student;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +17,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Demande::factory(10)->create();
+        Student::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
